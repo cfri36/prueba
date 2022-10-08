6ea0edc352cac02ae31fbef69efe4ec926be6286
cafra <cafra@192.168.1.2>
2022-10-07 23:03:22 -0500

    Nuevo Comentario --------

--- src/pruebaconect/PruebaConect.java
+++ src/pruebaconect/PruebaConect.java
@@ -6,7 +6,7 @@ public class PruebaConect {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Conex cn = new Conex();
+        Conex cn = new Conex(); //Prueba Conexion
         cn.getConnection();   
     }
 
